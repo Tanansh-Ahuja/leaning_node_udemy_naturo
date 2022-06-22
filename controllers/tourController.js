58290bddb1ea8cf0f57bdfbfb678c2d9f8c29304
Tanansh-Ahuja <tananshahuja17@gmail.com>
2022-06-22 10:56:04 +0530

    few changes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
-const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
-const APIFeatures = require('./../utils/apifeatures');
+const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -11,15 +11,14 @@ exports.aliasTopTours = (req, res, next) => {
 };
 
 exports.getAllTours = catchAsync(async (req, res, next) => {
-  //Execute Query
-  features = new APIFeatures(Tour.find(), req.query)
+  const features = new APIFeatures(Tour.find(), req.query)
     .filter()
     .sort()
     .limitFields()
-    .pagination();
+    .paginate();
   const tours = await features.query;
 
-  //Send Response
+  // SEND RESPONSE
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -31,10 +30,12 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
-  // Tour.findOne({_id: req.params.id})
+  // Tour.findOne({ _id: req.params.id })
+
   if (!tour) {
-    next(new AppError('No tour found with this id', 404));
+    return next(new AppError('No tour found with that ID', 404));
   }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -45,6 +46,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
 exports.createTour = catchAsync(async (req, res, next) => {
   const newTour = await Tour.create(req.body);
+
   res.status(201).json({
     status: 'success',
     data: {
@@ -58,9 +60,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true
   });
+
   if (!tour) {
-    next(new AppError('No tour found with this id', 404));
+    return next(new AppError('No tour found with that ID', 404));
   }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -71,9 +75,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
+
   if (!tour) {
-    next(new AppError('No tour found with this id', 404));
+    return next(new AppError('No tour found with that ID', 404));
   }
+
   res.status(204).json({
     status: 'success',
     data: null
@@ -87,9 +93,8 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
     },
     {
       $group: {
-        _id: '$difficulty',
-        // _id: '$ratingsAverage',
-        num: { $sum: 1 },
+        _id: { $toUpper: '$difficulty' },
+        numTours: { $sum: 1 },
         numRatings: { $sum: '$ratingsQuantity' },
         avgRating: { $avg: '$ratingsAverage' },
         avgPrice: { $avg: '$price' },
@@ -101,9 +106,10 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
       $sort: { avgPrice: 1 }
     }
     // {
-    //   $match: { _id: { $ne: 'easy' } }
+    //   $match: { _id: { $ne: 'EASY' } }
     // }
   ]);
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -112,8 +118,9 @@ exports.getTourStats = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getMonthyPlan = catchAsync(async (req, res, next) => {
-  const year = req.params.year * 1; //2021
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = req.params.year * 1; // 2021
+
   const plan = await Tour.aggregate([
     {
       $unwind: '$startDates'
@@ -145,9 +152,10 @@ exports.getMonthyPlan = catchAsync(async (req, res, next) => {
       $sort: { numTourStarts: -1 }
     },
     {
-      $limit: 6
+      $limit: 12
     }
   ]);
+
   res.status(200).json({
     status: 'success',
     data: {
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,69 +1,83 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+// const validator = require('validator');
+
 const tourSchema = new mongoose.Schema(
   {
-    //validator
-    //strings pe match bhi ek validator hai
     name: {
       type: String,
-      required: [true, 'tour must have a name'],
+      required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
-      maxLength: [40, 'A tour name must have 40 characters'],
-      minLength: [10, 'A tour name must have 10 characters']
-      // validate: [validator.isAlpha, 'Tourname must only have charaters']
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters']
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
-    slug: { type: String },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
     },
     maxGroupSize: {
       type: Number,
-      required: [true, 'A tour must have a max group size']
+      required: [true, 'A tour must have a group size']
     },
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
       enum: {
-        values: ['easy', 'medium', 'difficulty'],
-        message: 'Diffuclty level wrong,'
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
       }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.7,
-      min: [1, 'Rating must be 1 or more'],
-      max: [5, 'Rating must be greater than or equal to 5']
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
-    ratingsQuantity: { type: Number, default: 0 },
-    price: { type: Number, required: [true, 'tour must have a price'] },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
     priceDiscount: {
       type: Number,
       validate: {
         validator: function(val) {
-          //this will not run on update
-          // this keyword only work on new documents
-          return val < this.price; //100<200 true // 250<200 false
+          // this only points to current doc on NEW document creation
+          return val < this.price;
         },
-        message: `Pricediscount must be between 0 and ${this.price}`
+        message: 'Discount price ({VALUE}) should be below regular price'
       }
     },
     summary: {
       type: String,
       trim: true,
-      required: [true, 'A tour must have a summary']
+      required: [true, 'A tour must have a description']
     },
-    description: { type: String, trim: true },
+    description: {
+      type: String,
+      trim: true
+    },
     imageCover: {
       type: String,
-      required: [true, 'A tour must have a image cover']
+      required: [true, 'A tour must have a cover image']
     },
     images: [String],
-    createdAt: { type: Date, default: Date.now(), select: false },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
     startDates: [Date],
-    secretTour: { type: Boolean, default: false }
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -75,54 +89,44 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
-//MIDDLEWARES
-//DOCUMENT MIDDLEWARES: run before .save() and .create(), not .update()
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
-  // console.log(this);
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
 // tourSchema.pre('save', function(next) {
-//   console.log('will save document');
+//   console.log('Will save document...');
 //   next();
 // });
 
 // tourSchema.post('save', function(doc, next) {
 //   console.log(doc);
-//   // this.slug = slugify(this.name, { lower: true });
 //   next();
 // });
 
 // QUERY MIDDLEWARE
-////////////////////////////////
+// tourSchema.pre('find', function(next) {
 tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
+
   this.start = Date.now();
   next();
 });
-// tourSchema.pre('find', function(next) {
-//   this.find({ secretTour: { $ne: true } });
-//   next();
-// });
-// tourSchema.pre('findOne', function(next) {
-//   this.find({ secretTour: { $ne: true } });
-//   next();
-// });
+
 tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  // console.log(docs);
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   next();
 });
 
-////////////////////////////////////////////////////////////////
-//AGGREGATION MIDDLEWARE
-////////////////////////////////////////////////////////////////
-
+// AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function(next) {
-  console.log(this.pipleline);
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+
 module.exports = Tour;
